2261f19b0de0abce7cde04bff0af26d6ef9bd865
marisak98 <sanchezjordan.sh@gmail.com>
2024-04-17 15:59:48 -0500

    home page and login page demo add.

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+  return (
+    <main className="flex flex-col flex-1 justify-center px-20 text-center items-center w-full">
+      <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+        <div>Login Page</div>
+        {/*Login seccions*/}
+        <div>Logo Page</div>
+        {/*Logo and message*/}
+      </div>
+    </main>
+  );
+}
