d4989ab3555a23775b7e8e7d91abca878bc6a1ed
marisak98 <sanchezjordan.sh@gmail.com>
2024-04-23 16:09:07 -0500

    description on blueprint page

--- src/app/dashboard/blueprint/page.tsx
+++ src/app/dashboard/blueprint/page.tsx
@@ -1,7 +1,16 @@
 export default function Blueprint() {
   return (
-    <div>
-      <h1>Blueprint Page</h1>
-    </div>
+    <main className="p-4 sm:py-4 sm:px-4 sm:m-4">
+      <section className="flex items-center justify-between mb-4 md:mb-8 lg:mb-12">
+        <div className="space-y-1">
+          <h2 className="text-xl sm:text-2xl text-foreground font-semibold tracking-tight">
+            All Blueprint
+          </h2>
+          <p className="text-xs sm:text-sm text-muted-foreground">
+            Visualiza y administra todos los blueprint de tu organización.
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
