02ee9deeb06906d77b600cb4857ae858f2bcd33a
marisak98 <sanchezjordan.sh@gmail.com>
2024-04-26 01:39:22 -0500

    task table.

---
+++ src/app/dashboard/task/columns.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Checkbox } from "@/components/ui/checkbox";
+
+export enum Priority {
+  Baja = "Baja",
+  Media = "Media",
+  Alta = "Alta",
+}
+
+export enum Status {
+  EnProgreso = "En Progreso",
+  Cancelado = "Cancelado",
+  Completado = "Completado",
+  Pendiente = "Pendiente",
+  EnEspera = "En Espera",
+}
+export type Tasks = {
+  id: string;
+  task: string;
+  title: string;
+  priority: Priority;
+  status: Status;
+};
+
+export const columns: ColumnDef<Tasks>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Seleccionar las filas"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Seleccionar la fila"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "status",
+    header: "Estado",
+  },
+  {
+    accessorKey: "name",
+    header: "Nombre",
+  },
+  {
+    accessorKey: "area",
+    header: "Área",
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() == "asc")}
+        >
+          Correo
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "phone",
+    header: "Teléfono",
+  },
+  {
+    accessorKey: "proyects",
+    header: "Proyectos",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const project = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(project.id)}
+            >
+              Copiar ID del proyecto
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Ver Personal</DropdownMenuItem>
+            <DropdownMenuItem>Ver detalles del proyecto</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
---
+++ src/app/dashboard/task/page.tsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Users } from "lucide-react";
+import { Tasks, columns, Status, Priority } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getTask(): Promise<Tasks[]> {
+  return [
+    {
+      id: "1",
+      task: "TASK-1",
+      title: "Soldar la extructura para el proyecto 1",
+      status: Status.EnProgreso,
+      priority: Priority.Alta,
+    },
+    {
+      id: "2",
+      task: "TASK-2",
+      title: "Pulir la extructura para el proyecto 2",
+      status: Status.Completado,
+      priority: Priority.Media,
+    },
+    {
+      id: "3",
+      task: "TASK-3",
+      title: "Pintar la extructura para el proyecto 3",
+      status: Status.EnEspera,
+      priority: Priority.Baja,
+    },
+    {
+      id: "4",
+      task: "TASK-4",
+      title: "Pintar la extructura para el proyecto 4",
+      status: Status.Pendiente,
+      priority: Priority.Media,
+    },
+    {
+      id: "5",
+      task: "TASK-5",
+      title: "Doblar el tol para la cubierta",
+      status: Status.EnProgreso,
+      priority: Priority.Alta,
+    },
+    {
+      id: "6",
+      task: "TASK-6",
+      title: "Pulir la extructura para el proyecto 6",
+      status: Status.Completado,
+      priority: Priority.Media,
+    },
+  ];
+}
+
+export default async function Employees() {
+  const tasks = await getTask();
+  const data = await getTask();
+
+  return (
+    <main className="p-4 sm:px-4 sm:py-4">
+      <section className="flex items-center justify-between mb-4 md:mb-8 lg:mb-12">
+        <div className="space-y-1">
+          <h2 className="text-xl sm:text-2xl text-foreground font-semibold tracking-tight">
+            Tareas en Produccion.
+          </h2>
+          <p className="text-xs sm:text-sm text-muted-foreground">
+            Todas las Tareas.
+          </p>
+        </div>
+      </section>
+
+      <section className="flex items-center gap-2 mb-2">
+        {tasks && tasks.length > 0 ? (
+          <p className="text-sm text-gray-400 flex items-center gap-x-1">
+            <Users className="w-4 h-4" />
+            <span>{tasks.length} Numero de Tareas</span>
+          </p>
+        ) : null}
+      </section>
+
+      <Separator className="mb-5 bg-gray-200 dark:bg-gray-800" />
+
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </main>
+  );
+}
